3469e3694cc1f8b77ef02bdd2246107a29ceedb8
Yury Kuzmich <yurakuzmich@gmail.com>
2023-10-07 23:12:15 +0500

    forgot posts module :)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PostController } from './post.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, PostsModule],
   controllers: [AppController, PostController],
   providers: [AppService],
 })
